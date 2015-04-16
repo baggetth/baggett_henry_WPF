e8ba2c81bff7d2b1d950b8a7b7809c55f29cf086
baggetth <hebaggett@fullsail.edu>
2015-04-17 00:41:19 +0200

    Expressions Assignment
    Determined  the average temperature in centigrade to determent in it's
    warm enough to go outside

---
+++ assignments/Week Two/Baggett_Henry_Expressions_Assignment/js/script.js
@@ -0,0 +1,15 @@
+/*
+ Baggett_Henry_ Expressions Assignment
+ 04/16/2015
+ */
+
+
+// Determined  the average temperature in centigrade to determent in it's warm enough to go outside
+
+alert("The temperature for 5 days are 76, 80, 77, 67 and 74 Degrees in centigrade at 4 PM");// temperatures in fahrenheit
+
+var Temp1 = 76; // temperature 1
+ var Temp2 = 80;  // temperature 2
+var Temp3 = 77;  // temperature 3
+var Temp4 = 67;  // temperature 4
+var Temp5 = 74; // temperature 5
