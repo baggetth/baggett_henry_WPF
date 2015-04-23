914ccd903c84d35b4e1fddfffc70d70341a89004
baggetth <hebaggett@fullsail.edu>
2015-04-24 00:02:14 +0200

    Test Week 3 Assignment
    Test Week 3 Assignment

---
+++ assignments/Week 3/Baggett_Henry_Conditionals_Assignment/js/script.js
@@ -0,0 +1,20 @@
+/*
+ Baggett_Henry_ Conditionals Assignment
+ 04/22/2015
+ */
+
+
+//alert("Test");
+
+//Explain the propose 
+
+alert("Hello, this process will determent if you are allowed to work on the network .");  
+
+//Enter Last Name, First name 
+var name = prompt("Began by Entering Last Name, First Name"); 
+
+//console.log out name 
+console.log(name);
+
+  //Give the user an legal waring alert
+ alert("WARNING "+name+"! Making False Statements Could Result in Criminal Charges."); 
