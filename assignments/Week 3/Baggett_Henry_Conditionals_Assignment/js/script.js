141565fe0c83c1e56b3a3a988cc54942799be8fb
baggetth <hebaggett@fullsail.edu>
2015-04-24 00:04:41 +0200

    Waring alert and years of experience
    Waring alert and years of experience

--- assignments/Week 3/Baggett_Henry_Conditionals_Assignment/js/script.js
+++ assignments/Week 3/Baggett_Henry_Conditionals_Assignment/js/script.js
@@ -18,3 +18,11 @@ console.log(name);
 
   //Give the user an legal waring alert
  alert("WARNING "+name+"! Making False Statements Could Result in Criminal Charges."); 
+
+//This position requires 10 years of experience 
+alert("All our positions require a Minimum of 10 years Experience?"); 
+
+//years of experience
+var num1=prompt("Enter Years of Experience"); 
+var num2=Number("9");  
+
